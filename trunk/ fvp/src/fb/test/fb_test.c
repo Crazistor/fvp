4f8a8deb546a9c248e337b25a165a7668fbefe9e
dcdcmin@gmail.com <dcdcmin@gmail.com@82d9bda7-8901-ce7c-64af-18dc06c08770>
2011-02-12 07:23:17 +0000

    add fb dir

---
+++ trunk/ fvp/src/fb/test/fb_test.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include"framebuffer.h"
+#include"fvp_common.h"
+
+
+int main(int argc, char *argv[])
+{
+	FrameBuffer *thiz  = NULL;
+
+	thiz = framebuffer_create("/dev/fb0");
+	assert(thiz != NULL);
+	assert(framebuffer_init(thiz, 1024, 768) == RET_OK);
+
+	getchar();
+	framebuffer_set_alpha(thiz, 0x00);
+
+	getchar();
+	framebuffer_destroy(thiz);
+	
+	return 0;
+}
+
