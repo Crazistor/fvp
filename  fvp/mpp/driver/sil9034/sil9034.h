1fba410a9af1cc68fc505e4c0f1eb2f898e36f27
dcdcmin <dcdcmin@gmail.com>
2011-02-22 09:53:52 +0000

    delete some no used files

---  fvp/mpp/driver/sil9034/sil9034.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SIL9034_H__
-#define __SIL9034_H__
-
-#define SIL9034_I2C_ADDR_A  0x72
-#define SIL9034_I2C_ADDR_B  0x7a
-
-#endif  /* __SIL9034_H__ */
-
---  fvp/mpp/readme.txt
+++
@@ -1,1 +0,0 @@
-relatived with sdk/toolchain
