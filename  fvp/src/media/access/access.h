f8495aa5eb2be6ea159f4534bd3cd31c8e560d06
dcdcmin <dcdcmin@gmail.com>
2011-03-31 13:47:03 +0000

    add access.h to project

---
+++  fvp/src/media/access/access.h
@@ -0,0 +1,49 @@
+#ifndef ACCESS_H
+#define ACCESS_H
+
+
+struct _Access;
+typedef struct _Access Access;
+
+typedef block*(*access_read)(Access *thiz);
+
+typedef void (*access_destroy)(Access *thiz);
+struct _Access
+{
+    
+    access_destroy destroy;
+    char priv[0];
+};
+
+
+void access_seek()
+{
+
+    return thiz->seek();
+}
+
+void access_control()
+{
+
+    return thiz->control();
+}
+
+Block *access_read(Access *thiz)
+{
+
+    return thiz->read(Access *thiz);
+}
+
+
+void access_destroy(Access *thiz)
+{
+    if(thiz)
+    {
+        thiz->destroy(thiz);
+    }
+}
+
+
+
+
+#endif /*ACCESS_H*/
