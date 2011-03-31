c84df7983dfd7fb45dabe0b2559ba1bb8446549f
dcdcmin@gmail.com <dcdcmin@gmail.com@82d9bda7-8901-ce7c-64af-18dc06c08770>
2011-03-31 13:26:45 +0000

    add a  media access dir

---
+++ trunk/ fvp/src/media/access/access.h
@@ -0,0 +1,28 @@
+
+
+
+typedef 
+
+
+
+block *access_stream_read()
+{
+
+    return block;
+}
+
+
+
+access *access_stream_seek();
+{
+    
+    return ;
+}
+
+
+access *access_control()
+{
+
+
+    return;
+}
---
+++ trunk/ fvp/src/media/access/access_file.h
@@ -0,0 +1,19 @@
+#ifndef ACCESS_FILE_H
+#define ACCESS_FILE_H
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+
+void read()
+    seek();
+    control();
+
+
+#ifdef __cplusplus
+}
+#endif 
+
+#endif /*ACCESS_FILE_H*/
