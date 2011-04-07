5c530dfb2b3d50182317355eb49dfbed048fb98c
dcdcmin@gmail.com <dcdcmin@gmail.com@82d9bda7-8901-ce7c-64af-18dc06c08770>
2011-04-07 09:56:31 +0000

    add access file module

--- trunk/ fvp/src/media/access/access_file.h
+++ trunk/ fvp/src/media/access/access_file.h
@@ -1,5 +1,47 @@
+/*
+ * File: access_file.h
+ * Author:  zhoumin  <dcdcmin@gmail.com>
+ * Brief:   
+ *
+ * Copyright (c) 2010 - 2013  zhoumin <dcdcmin@gmail.com>>
+ *
+ * Licensed under the Academic Free License version 2.1
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ * History:
+ * ================================================================
+ * 2011-04-07 zhoumin <dcdcmin@gmail.com> created
+ *
+ */
+ 
 #ifndef ACCESS_FILE_H
 #define ACCESS_FILE_H
+
+#include"access.h"
+
+
+
+/*
+ * create 
+ */
+Access *access_file_create();
+
+
 
 
 #ifdef __cplusplus
@@ -7,13 +49,10 @@ extern "C"{
 #endif 
 
 
-void read()
-    seek();
-    control();
-
-
 #ifdef __cplusplus
 }
 #endif 
 
 #endif /*ACCESS_FILE_H*/
+
+
