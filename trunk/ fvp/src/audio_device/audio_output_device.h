951e41218d863b7671bf76d144f4971900bb1d83
dcdcmin@gmail.com <dcdcmin@gmail.com@82d9bda7-8901-ce7c-64af-18dc06c08770>
2011-05-17 14:13:27 +0000

    add audio device dir

---
+++ trunk/ fvp/src/audio_device/audio_output_device.h
@@ -0,0 +1,68 @@
+/*
+ * File: audio_output_device.h
+ * Author:  zhoumin  <dcdcmin@gmail.com>
+ * Brief:   
+ *
+ * Copyright (c) 2010 - 2013  zhoumin <dcdcmin@gmail.com>>
+ *
+ * Licensed under the Academic Free License version 2.1
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+/*
+ * History:
+ * ================================================================
+ * 2011-5-17 zhoumin <dcdcmin@gmail.com> created
+ *
+ */
+
+#ifndef AUDIO_OUTPUT_DEVICE_H
+#define AUDIO_OUTPUT_DEVICE_H
+
+#include"fvp_common.h"
+#include"fvp_typedef.h"
+
+
+#ifdef __cplusplus
+extern "C"
+#endif 
+
+
+struct _AudioOutputDevice;
+typedef struct _AudioOutputDevice AudioOutputDevice;
+
+
+
+
+
+AudioOutputDevice *audio_output_device_create(AUDIO_DEV ao_dev_id,
+														int ao_channel, 
+														PAYLOAD_TYPE_E playload_type);
+
+
+
+void audio_output_device_destroy(AudioOutputDevice *thiz);
+
+
+
+
+#ifdef __cplusplus
+extern "C"
+#endif 
+
+
+
+#endif /*AUDIO_OUTPUT_DEVICE_H*/
+ 
